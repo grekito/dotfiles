4baf13c394efaf251982dc666067440fb97c85c2
grekito <costamarudes@gmail.com>
2024-05-04 00:33:28 -0300

    changes script

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ mkdir -p ~/.config/nvim/lua/costa/
 cp .config/nvim/lua/costa/lazy.lua ~/.config/nvim/lua/costa/lazy.lua
 cp .config/nvim/init.lua ~/.config/nvim/
 cp -r .config/nvim/lua/costa/core ~/.config/nvim/lua/costa/
+cp -r .config/nvim/lua/costa/plugins ~/.config/nvim/lua/costa/
 
 nvim
 
