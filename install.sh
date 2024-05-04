3017720eb9b4bd22494c9c85f57de8c5abeb5a9a
grekito <costamarudes@gmail.com>
2024-05-04 02:04:39 -0300

    changes script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 sudo pacman --noconfirm --needed -Syu neovim zsh tmux stow
 
-cp -r .zsh ~/ && cp .zshenv ~/ 
+cp -r .zsh ~/ && cp .zshenv ~/ && mkdir ~/Pictures/ && cp ARCH.jpg ~/Pictures/ 
+
+nitrogen ~/Pictures/ARCH.jpg
 
 cd .config && cp -r kitty ~/.config/ && cp -r nvim ~/.config/ && cp -r qtile ~/.config/ 
 
