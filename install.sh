12d0d94d2abc1afc90133893f481789a35e7cb19
grekito <costamarudes@gmail.com>
2024-05-04 01:53:38 -0300

    changes script

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@ sudo pacman --noconfirm --needed -Syu neovim zsh tmux stow
 
 cp -r .zsh ~/ && cp .zshenv ~/ 
 
-cd .config && cp -r kitty ~/.config/ && cp nvim ~/.config/ && cp qtile ~/.config/ 
+cd .config && cp -r kitty ~/.config/ && cp -r nvim ~/.config/ && cp -r qtile ~/.config/ 
 
 cd .. && stow --adopt .
-
-
-
