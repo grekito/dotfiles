1dddd8f146d612fbe24083742deb89c784f701d5
grekito <costamarudes@gmail.com>
2024-05-04 02:19:52 -0300

    changes

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 sudo pacman --noconfirm --needed -Syu neovim zsh tmux stow
 
-cp -r .zsh ~/ && cp .zshenv ~/ && mkdir ~/Pictures/ && cp ARCH.jpg ~/Pictures/ 
+cp -r .zsh ~/ && cp .zshenv ~/ && mkdir ~/Pictures/ && cp arch.jpg ~/Pictures/arch.jpg
 
-nitrogen ~/Pictures/ARCH.jpg
+nitrogen ~/Pictures/arch.jpg
 
 cd .config && cp -r kitty ~/.config/ && cp -r nvim ~/.config/ && cp -r qtile ~/.config/ 
 
 cd .. && stow --adopt .
+
+mkdir -p ~/.config/tmux/plugins && cd ~/.config/tmux/plugins/ && git clone https://github.com/tmux-plugins/tpm
