13c63baf34a273b604c3065f2c30a91661def041
grekito <costamarudes@gmail.com>
2024-05-04 02:55:15 -0300

    changes

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
-sudo pacman --noconfirm --needed -Syu neovim zsh tmux stow
+sudo pacman --noconfirm --needed -Syu neovim zsh tmux stow eza
+
+chsh -s /usr/bin/zsh
 
 cp -r .zsh ~/ && cp .zshenv ~/ && mkdir ~/Pictures/ && cp arch.jpg ~/Pictures/arch.jpg
 
-nitrogen ~/Pictures/arch.jpg
+nitrogen ~/Pictures/
 
-cd .config && cp -r kitty ~/.config/ && cp -r nvim ~/.config/ && cp -r qtile ~/.config/ 
+cd .config && cp -r kitty ~/.config/ && cp -r nvim ~/.config/ && cp -r qtile ~/.config/ && cp tmux/tmux.conf ~/.config/tmux/tmux.conf
 
 mkdir -p ~/.config/tmux/plugins && cd ~/.config/tmux/plugins/ && git clone https://github.com/tmux-plugins/tpm
+
+mkdir -p ~/.zsh/plugins/ && cd ~/.zsh/plugins/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions
