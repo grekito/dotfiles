edc07aadf44ce26509013fba41a078baf96a357d
grekito <costamarudes@gmail.com>
2024-05-04 00:12:23 -0300

    changes script

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 sudo pacman --noconfirm --needed -Syu neovim
 
+mkdir -p ~/.config/nvim/lua/costa/
+
+cp .config/nvim/lua/costa/init.lua ~/.config/nvim/lua/costa/
+
+nvim
+
+vim.cmd :Lazy
+vim.cmd :q
+
 cp -r .config/nvim ~/.config/nvim
 
